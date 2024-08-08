0bebd0d603a7a037d1d213f04bbd7ba20bacc830
Greg Bacchus <1761608+gregbacchus@users.noreply.github.com>
2024-08-09 09:48:45 +1200

    breadcrumb color

--- packages/@geee-be/react-twui/src/components/Breadcrumb/Breadcrumb.tsx
+++ packages/@geee-be/react-twui/src/components/Breadcrumb/Breadcrumb.tsx
@@ -18,7 +18,7 @@ const BreadcrumbList = React.forwardRef<
   <ol
     ref={ref}
     className={cn(
-      'flex flex-wrap items-center gap-1.5 break-words text-sm text-muted-foreground sm:gap-2.5',
+      'flex flex-wrap items-center gap-1.5 break-words text-sm text-paper-fg/75 sm:gap-2.5',
       className,
     )}
     {...props}
@@ -49,7 +49,7 @@ const BreadcrumbLink = React.forwardRef<
   return (
     <Comp
       ref={ref}
-      className={cn('transition-colors hover:text-foreground', className)}
+      className={cn('transition-colors hover:text-paper-fg/100', className)}
       {...props}
     />
   );
@@ -65,7 +65,7 @@ const BreadcrumbPage = React.forwardRef<
     role="link"
     aria-disabled="true"
     aria-current="page"
-    className={cn('font-medium text-foreground', className)}
+    className={cn('font-medium text-paper-fg/100', className)}
     {...props}
   />
 ));
