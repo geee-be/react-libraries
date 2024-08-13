f4829ef49107260cfdcdbacb84b5b79c1a1d1961
Greg Bacchus <1761608+gregbacchus@users.noreply.github.com>
2024-08-14 05:46:44 +1000

    Missed passing gap

--- packages/@geee-be/react-twui/src/components/Card/CardContent.tsx
+++ packages/@geee-be/react-twui/src/components/Card/CardContent.tsx
@@ -10,14 +10,14 @@ export const CardContent = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement> &
     VariantProps<typeof cardContentVariants>
->(({ className, children, gutters, ...props }, ref) => {
+>(({ className, children, gap, gutters, ...props }, ref) => {
   const Component = isReactElement(children) ? Slot : 'div';
 
   return (
     <div data-component="CardContent" className={className}>
       <Component
         ref={ref}
-        className={cn('Card-content', cardContentVariants({ gutters }))}
+        className={cn('Card-content', cardContentVariants({ gap, gutters }))}
         {...props}
       >
         {children}
