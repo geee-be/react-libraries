6c123a71bce72d30da6e2223c90cdb16a1ef6806
Greg Bacchus <1761608+gregbacchus@users.noreply.github.com>
2024-08-21 22:21:39 +1200

    missing exports

--- packages/@geee-be/react-twui/src/components/InputOtp/index.ts
+++ packages/@geee-be/react-twui/src/components/InputOtp/index.ts
@@ -1,6 +1,8 @@
+export { FormInputOtp } from './FormInputOtp.js';
 export {
   InputOtp,
   InputOtpGroup,
   InputOtpSeparator,
   InputOtpSlot,
 } from './InputOtp.js';
+export type { InputOtpElement, InputOtpProps } from './InputOtp.js';
