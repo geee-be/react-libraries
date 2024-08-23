8dfee894d70a0a98c86f1db66f004885c9f6cf04
Greg Bacchus <1761608+gregbacchus@users.noreply.github.com>
2024-08-23 18:42:18 +1200

    thumb style

---
+++ packages/@geee-be/react-twui/src/components/ScrollArea/ScrollArea.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ScrollArea, ScrollBar } from './index.js';
+
+const meta = {
+  component: ScrollArea,
+  argTypes: {},
+} satisfies Meta<typeof ScrollArea>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export interface Artwork {
+  artist: string;
+  art: string;
+}
+
+export const Default: Story = {
+  args: {
+    className: 'h-[200px] w-[350px] rounded-md border p-4',
+    children: (
+      <>
+        <p>
+          Jokester began sneaking into the castle in the middle of the night and
+          leaving jokes all over the place: under the king's pillow, in his
+          soup, even in the royal toilet. The king was furious, but he couldn't
+          seem to stop Jokester. And then, one day, the people of the kingdom
+          discovered that the jokes left by Jokester were so funny that they
+          couldn't help but laugh. And once they started laughing, they couldn't
+          stop.
+        </p>
+        <ScrollBar orientation="vertical" />
+      </>
+    ),
+  },
+};
--- packages/@geee-be/react-twui/src/components/ScrollArea/ScrollArea.tsx
+++ packages/@geee-be/react-twui/src/components/ScrollArea/ScrollArea.tsx
@@ -24,22 +24,29 @@ ScrollArea.displayName = ScrollAreaPrimitive.Root.displayName;
 
 const ScrollBar = React.forwardRef<
   React.ElementRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>,
-  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>
->(({ className, orientation = 'vertical', ...props }, ref) => (
+  React.ComponentPropsWithoutRef<
+    typeof ScrollAreaPrimitive.ScrollAreaScrollbar
+  > & { thumbClassName?: string }
+>(({ className, thumbClassName, orientation = 'vertical', ...props }, ref) => (
   <ScrollAreaPrimitive.ScrollAreaScrollbar
     ref={ref}
     orientation={orientation}
     className={cn(
       'flex touch-none select-none transition-colors',
       orientation === 'vertical' &&
-        'h-full w-2.5 border-l border-l-transparent p-[1px]',
+        'h-full w-3 border-l border-l-transparent p-[1px]',
       orientation === 'horizontal' &&
-        'h-2.5 flex-col border-t border-t-transparent p-[1px]',
+        'h-3 flex-col border-t border-t-transparent p-[1px]',
       className,
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
+    <ScrollAreaPrimitive.ScrollAreaThumb
+      className={cn(
+        'relative flex-1 rounded-full bg-default/80',
+        thumbClassName,
+      )}
+    />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ));
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName;
