a78117b6d4657926e7711bfc56642e7c8d4322c0
Greg Bacchus <1761608+gregbacchus@users.noreply.github.com>
2024-09-03 22:53:03 +1200

    Revert sub components

--- packages/@geee-be/react-twui/src/components/Card/index.ts
+++ packages/@geee-be/react-twui/src/components/Card/index.ts
@@ -1,17 +1,5 @@
-import { withSubComponents } from '@geee-be/react-utils';
-
-import { Card as CardRoot } from './Card.js';
-import { CardContent } from './CardContent';
-import { CardFooter } from './CardFooter';
-import { CardHeader } from './CardHeader';
+export { Card } from './Card.js';
 export type { CardProps } from './Card.js';
-
 export { CardContent } from './CardContent';
 export { CardFooter } from './CardFooter';
 export { CardHeader } from './CardHeader';
-
-export const Card = withSubComponents(CardRoot, {
-  Content: CardContent,
-  Footer: CardFooter,
-  Header: CardHeader,
-});
