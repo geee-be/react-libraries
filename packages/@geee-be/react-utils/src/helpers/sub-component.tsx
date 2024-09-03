193df9b5e884465d34af9da0a9918b232302b9d1
Greg Bacchus <1761608+gregbacchus@users.noreply.github.com>
2024-09-03 22:28:10 +1200

    attempt to fix build error

--- packages/@geee-be/react-utils/src/helpers/sub-component.tsx
+++ packages/@geee-be/react-utils/src/helpers/sub-component.tsx
@@ -1,6 +1,6 @@
-import { type ComponentType, forwardRef } from 'react';
+import { type ComponentType, forwardRef, type Ref } from 'react';
 
-export const withSubComponents = <P extends {}, S>(
+export const withSubComponents = <P extends { ref?: Ref<P> }, S>(
   RootComponent: ComponentType<P>,
   subComponents: S,
 ) =>
