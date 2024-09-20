a49e90818dffb9e59fe63316bff428e8c098969c
Greg Bacchus <1761608+gregbacchus@users.noreply.github.com>
2024-09-20 18:56:08 +1200

    forwardRef

--- packages/@geee-be/react-twui/src/components/InputImage/InputFile.tsx
+++ packages/@geee-be/react-twui/src/components/InputImage/InputFile.tsx
@@ -1,49 +1,49 @@
 'use client';
 
 import type { VariantProps } from 'cva';
-import type { FC, PropsWithChildren } from 'react';
+import { forwardRef, type PropsWithChildren } from 'react';
 import { useDropzone, type DropzoneOptions } from 'react-dropzone-esm';
 import { cn } from '../../helpers/utils';
 import { borderVariants } from './variants';
 
 export type InputFileProps = VariantProps<typeof borderVariants> &
   Omit<DropzoneOptions, 'maxFiles' | 'minFiles'> &
-  Pick<React.HTMLProps<HTMLElement>, 'id' | 'ref' | 'className'> & {
+  Pick<React.HTMLProps<HTMLElement>, 'id' | 'className'> & {
     onChange?: (file?: File) => void;
   };
 
-export const InputFile: FC<PropsWithChildren<InputFileProps>> = ({
-  id,
-  ref,
-  children,
-  className,
-  onChange: onFileSelect,
-  shape,
-  ...props
-}) => {
-  const { getRootProps, getInputProps, isDragAccept, isDragReject } =
-    useDropzone({
-      ...props,
-      onDropAccepted: (files, e) => {
-        onFileSelect?.(files.length ? files[0] : undefined);
-        props.onDropAccepted?.(files, e);
-      },
-      maxFiles: 1,
-    });
+export const InputFile = forwardRef<
+  HTMLInputElement,
+  PropsWithChildren<InputFileProps>
+>(
+  (
+    { id, children, className, onChange: onFileSelect, shape, ...props },
+    ref,
+  ) => {
+    const { getRootProps, getInputProps, isDragAccept, isDragReject } =
+      useDropzone({
+        ...props,
+        onDropAccepted: (files, e) => {
+          onFileSelect?.(files.length ? files[0] : undefined);
+          props.onDropAccepted?.(files, e);
+        },
+        maxFiles: 1,
+      });
 
-  return (
-    <label
-      {...getRootProps({
-        className: cn(
-          borderVariants({ shape }),
-          isDragAccept && 'border-success bg-success/10',
-          isDragReject && 'border-error bg-error/10 cursor-not-allowed',
-          className,
-        ),
-      })}
-    >
-      <input id={id} type="file" {...getInputProps()} />
-      {children}
-    </label>
-  );
-};
+    return (
+      <label
+        {...getRootProps({
+          className: cn(
+            borderVariants({ shape }),
+            isDragAccept && 'border-success bg-success/10',
+            isDragReject && 'border-error bg-error/10 cursor-not-allowed',
+            className,
+          ),
+        })}
+      >
+        <input id={id} ref={ref} type="file" {...getInputProps()} />
+        {children}
+      </label>
+    );
+  },
+);
--- packages/@geee-be/react-twui/src/components/InputImage/InputImage.tsx
+++ packages/@geee-be/react-twui/src/components/InputImage/InputImage.tsx
@@ -4,11 +4,11 @@ import { useMediaQuery } from '@uidotdev/usehooks';
 import type { VariantProps } from 'cva';
 import { ChevronLeft, CircleCheckBig } from 'lucide-react';
 import {
+  forwardRef,
   useCallback,
   useMemo,
   useRef,
   useState,
-  type FC,
   type ReactNode,
 } from 'react';
 import type { CropperProps, CropperRef } from 'react-advanced-cropper';
@@ -25,10 +25,7 @@ import type { borderVariants } from './variants';
 
 export type InputImageProps = VariantProps<typeof borderVariants> &
   Omit<DropzoneOptions, 'maxFiles' | 'minFiles' | 'placeholder'> &
-  Pick<
-    React.HTMLProps<HTMLElement>,
-    'id' | 'ref' | 'className' | 'onChange'
-  > & {
+  Pick<React.HTMLProps<HTMLElement>, 'id' | 'className' | 'onChange'> & {
     cropperProps?: Pick<CropperProps, 'stencilComponent' | 'stencilProps'>;
     cropTitle?: ReactNode;
     discardImageTitle?: ReactNode;
@@ -40,147 +37,158 @@ export type InputImageProps = VariantProps<typeof borderVariants> &
     value?: Blob;
   };
 
-export const InputImage: FC<InputImageProps> = ({
-  id,
-  ref,
-  className,
-  accept,
-  cropperProps,
-  cropTitle = 'Crop image',
-  disabled,
-  discardImageTitle = 'Discard',
-  imageSpec,
-  onChange,
-  outputMimeType,
-  placeholder,
-  useImageTitle = 'Use image',
-  shape,
-  value,
-}) => {
-  const [file, setFile] = useState<File | undefined>();
-  const [showCropper, setShowCropper] = useState(false);
-  const [rendering, setRendering] = useState(false);
-  const [uncontrolledValue, setUncontrolledValue] = useState<Blob>();
-  const narrow = useMediaQuery('@container (min-width: 42rem /* 672px */)');
-  const cropperRef = useRef<CropperRef>(null);
-
-  const displayValue = value ?? uncontrolledValue;
-
-  const aspectRatio = useMemo(
-    () =>
-      'aspectRatio' in imageSpec
-        ? imageSpec.aspectRatio
-        : {
-            minimum: imageSpec.width / imageSpec.height,
-            maximum: imageSpec.width / imageSpec.height,
-          },
-    [imageSpec],
-  );
-
-  const handleUseImage = useCallback(() => {
-    const canvas = cropperRef.current?.getCanvas();
-    if (!canvas) return;
-
-    setRendering(true);
-
-    const exportSize = finalSize(
-      { width: canvas.width, height: canvas.height },
+export const InputImage = forwardRef<HTMLInputElement, InputImageProps>(
+  (
+    {
+      id,
+      className,
+      accept,
+      cropperProps,
+      cropTitle = 'Crop image',
+      disabled,
+      discardImageTitle = 'Discard',
       imageSpec,
+      onChange,
+      outputMimeType,
+      placeholder,
+      useImageTitle = 'Use image',
+      shape,
+      value,
+    },
+    ref,
+  ) => {
+    const [file, setFile] = useState<File | undefined>();
+    const [showCropper, setShowCropper] = useState(false);
+    const [rendering, setRendering] = useState(false);
+    const [uncontrolledValue, setUncontrolledValue] = useState<Blob>();
+    const narrow = useMediaQuery('@container (min-width: 42rem /* 672px */)');
+    const cropperRef = useRef<CropperRef>(null);
+
+    const displayValue = value ?? uncontrolledValue;
+
+    const aspectRatio = useMemo(
+      () =>
+        'aspectRatio' in imageSpec
+          ? imageSpec.aspectRatio
+          : {
+              minimum: imageSpec.width / imageSpec.height,
+              maximum: imageSpec.width / imageSpec.height,
+            },
+      [imageSpec],
     );
 
-    exportImage(canvas, exportSize, outputMimeType)
-      .then((blob) => {
-        onChange?.(blob);
-        setUncontrolledValue(blob);
-        setFile(undefined);
-        setShowCropper(false);
-      })
-      .finally(() => {
-        setRendering(false);
-      });
-  }, [imageSpec, outputMimeType, onChange]);
+    const handleUseImage = useCallback(() => {
+      const canvas = cropperRef.current?.getCanvas();
+      if (!canvas) return;
 
-  const cropper = useMemo(
-    () =>
-      file && (
-        <Cropper
-          ref={cropperRef}
-          src={URL.createObjectURL(file)}
-          aspectRatio={aspectRatio ?? { minimum: 1, maximum: 1 }}
-          className="!max-h-[calc(100vh-180px)]"
-          {...cropperProps}
-        />
-      ),
-    [file, aspectRatio, cropperProps],
-  );
+      setRendering(true);
 
-  return (
-    <>
-      <InputFile
-        id={id}
-        ref={ref}
-        accept={
-          accept ?? {
-            'image/gif': ['.gif'],
-            'image/png': ['.png'],
-            'image/jpeg': ['.jpg', '.jpeg'],
-            'image/svg+xml': ['.svg'],
-            'image/webp': ['.webp'],
+      const exportSize = finalSize(
+        { width: canvas.width, height: canvas.height },
+        imageSpec,
+      );
+
+      exportImage(canvas, exportSize, outputMimeType)
+        .then((blob) => {
+          onChange?.(blob);
+          setUncontrolledValue(blob);
+          setFile(undefined);
+          setShowCropper(false);
+        })
+        .finally(() => {
+          setRendering(false);
+        });
+    }, [imageSpec, outputMimeType, onChange]);
+
+    const cropper = useMemo(
+      () =>
+        file && (
+          <Cropper
+            ref={cropperRef}
+            src={URL.createObjectURL(file)}
+            aspectRatio={aspectRatio ?? { minimum: 1, maximum: 1 }}
+            className="!max-h-[calc(100vh-180px)]"
+            {...cropperProps}
+          />
+        ),
+      [file, aspectRatio, cropperProps],
+    );
+
+    return (
+      <>
+        <InputFile
+          id={id}
+          ref={ref}
+          accept={
+            accept ?? {
+              'image/gif': ['.gif'],
+              'image/png': ['.png'],
+              'image/jpeg': ['.jpg', '.jpeg'],
+              'image/svg+xml': ['.svg'],
+              'image/webp': ['.webp'],
+            }
           }
-        }
-        className={cn(displayValue && 'border-solid p-0', className)}
-        disabled={disabled}
-        onChange={(selectedFile) => {
-          setFile(selectedFile);
-          setShowCropper(!!selectedFile);
-        }}
-        shape={shape}
-      >
-        {displayValue ? (
-          <img src={URL.createObjectURL(displayValue)} alt="selected content" />
-        ) : (
-          placeholder
-        )}
-      </InputFile>
-      <Dialog
-        open={showCropper && !disabled}
-        onOpenChange={(open) => {
-          setShowCropper(open);
-          if (!open) {
-            setFile(undefined);
-          }
-        }}
-      >
-        <DialogContent aria-describedby={undefined}>
-          <DialogTitle>{cropTitle}</DialogTitle>
-          {cropper}
-          <div className="flex gap-3 justify-end @container">
-            <Button
-              variant="outline"
-              isIconOnly={narrow}
-              before={<ChevronLeft />}
-              disabled={rendering}
-              onClick={() => {
-                setShowCropper(false);
-              }}
-            >
-              <span className="sr-only @xs:not-sr-only">
-                {discardImageTitle}
-              </span>
-            </Button>
-            <Button
-              color="primary"
-              before={
-                !rendering ? <CircleCheckBig /> : <Spinner color="foreground" />
-              }
-              disabled={rendering}
-              onClick={() => handleUseImage()}
-            >
-              {useImageTitle}
-            </Button>
-          </div>
-        </DialogContent>
-      </Dialog>
-    </>
-  );
-};
+          className={cn(displayValue && 'border-solid p-0', className)}
+          disabled={disabled}
+          onChange={(selectedFile) => {
+            setFile(selectedFile);
+            setShowCropper(!!selectedFile);
+          }}
+          shape={shape}
+        >
+          {displayValue ? (
+            <img
+              src={URL.createObjectURL(displayValue)}
+              alt="selected content"
+            />
+          ) : (
+            placeholder
+          )}
+        </InputFile>
+        <Dialog
+          open={showCropper && !disabled}
+          onOpenChange={(open) => {
+            setShowCropper(open);
+            if (!open) {
+              setFile(undefined);
+            }
+          }}
+        >
+          <DialogContent aria-describedby={undefined}>
+            <DialogTitle>{cropTitle}</DialogTitle>
+            {cropper}
+            <div className="flex gap-3 justify-end @container">
+              <Button
+                variant="outline"
+                isIconOnly={narrow}
+                before={<ChevronLeft />}
+                disabled={rendering}
+                onClick={() => {
+                  setShowCropper(false);
+                }}
+              >
+                <span className="sr-only @xs:not-sr-only">
+                  {discardImageTitle}
+                </span>
+              </Button>
+              <Button
+                color="primary"
+                before={
+                  !rendering ? (
+                    <CircleCheckBig />
+                  ) : (
+                    <Spinner color="foreground" />
+                  )
+                }
+                disabled={rendering}
+                onClick={() => handleUseImage()}
+              >
+                {useImageTitle}
+              </Button>
+            </div>
+          </DialogContent>
+        </Dialog>
+      </>
+    );
+  },
+);
