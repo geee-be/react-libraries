edddc674afaf580bea4981acf0cd184700a181ed
Greg Bacchus <1761608+gregbacchus@users.noreply.github.com>
2024-11-21 09:18:36 +1300

    improved circle skeleton

--- packages/@geee-be/react-twui/src/components/Skeleton/variants.ts
+++ packages/@geee-be/react-twui/src/components/Skeleton/variants.ts
@@ -4,7 +4,7 @@ export const skeletonVariants = cva({
   base: ['animate-pulse rounded-md bg-skeleton'],
   variants: {
     shape: {
-      circle: 'rounded-full',
+      circle: 'rounded-full aspect-square min-w-[1.5rem] min-h-[1.5rem]',
       input: 'h-[2.5rem] w-full rounded-lg',
       label: 'h-[1.5rem] w-[30%] bg-skeleton/50',
       helper: 'h-[1.5rem] w-[60%] bg-skeleton/50',
