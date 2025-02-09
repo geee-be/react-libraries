1bf02d92a51d415434ba69edf5075b6e9f92d483
Greg Bacchus <1761608+gregbacchus@users.noreply.github.com>
2025-02-09 19:54:20 +1300

    fix gaps in breadcrumb list

--- packages/@geee-be/react-twui/src/components/Breadcrumb/Breadcrumb.tsx
+++ packages/@geee-be/react-twui/src/components/Breadcrumb/Breadcrumb.tsx
@@ -18,7 +18,7 @@ const BreadcrumbList = React.forwardRef<
   <ol
     ref={ref}
     className={cn(
-      'flex flex-wrap items-center gap-1.5 break-words text-sm text-paper-fg/75 sm:gap-2.5',
+      'flex flex-wrap items-center gap-y-0 gap-x-1.5 break-words text-sm text-paper-fg/75 sm:gap-x-2.5',
       className,
     )}
     {...props}
