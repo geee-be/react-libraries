ceb19b1cdfc9d3c07abcfb9bc439a656c514bffe
Greg Bacchus <1761608+gregbacchus@users.noreply.github.com>
2025-03-31 08:24:05 +1300

    NoWrap option for WithIcon

--- packages/@geee-be/react-twui/src/components/WithIcon/index.tsx
+++ packages/@geee-be/react-twui/src/components/WithIcon/index.tsx
@@ -5,6 +5,7 @@ interface Props {
   'data-content'?: string;
   iconBefore?: ReactNode;
   iconAfter?: ReactNode;
+  noWrap?: boolean;
 }
 
 export const WithIcon: FC<PropsWithChildren<Props>> = ({
@@ -13,19 +14,34 @@ export const WithIcon: FC<PropsWithChildren<Props>> = ({
   'data-content': dataContent,
   iconBefore,
   iconAfter,
+  noWrap,
 }) => (
   <div
     data-content={dataContent}
     className={`inline align-middle leading-[1em] ${className ?? ''}`}
   >
     {iconBefore && (
-      <div className="inline-block align-middle whitespace-pre-wrap mr-[0.5em] w-[1em] h-[1em] [&>svg]:max-w-[1em] [&>svg]:max-h-[1em]">
+      <div
+        className={`inline-block align-middle ${
+          noWrap ? 'whitespace-nowrap' : 'whitespace-pre-wrap'
+        } mr-[0.5em] w-[1em] h-[1em] [&>svg]:max-w-[1em] [&>svg]:max-h-[1em]`}
+      >
         {iconBefore}
       </div>
     )}
-    <div className="inline align-middle whitespace-pre-wrap">{children}</div>
+    <div
+      className={`inline align-middle ${
+        noWrap ? 'whitespace-nowrap' : 'whitespace-pre-wrap'
+      }`}
+    >
+      {children}
+    </div>
     {iconAfter && (
-      <div className="inline-block align-middle whitespace-pre-wrap ml-[0.5em] w-[1em] h-[1em] [&>svg]:max-w-[1em] [&>svg]:max-h-[1em]">
+      <div
+        className={`inline-block align-middle ${
+          noWrap ? 'whitespace-nowrap' : 'whitespace-pre-wrap'
+        } ml-[0.5em] w-[1em] h-[1em] [&>svg]:max-w-[1em] [&>svg]:max-h-[1em]`}
+      >
         {iconAfter}
       </div>
     )}
