3642f0bb2c2eefbf54655711a44d2462f747b00f
Greg Bacchus <1761608+gregbacchus@users.noreply.github.com>
2025-09-06 10:31:27 +1200

    compound card

--- packages/@geee-be/react-twui/src/components/Card/index.ts
+++ packages/@geee-be/react-twui/src/components/Card/index.ts
@@ -1,5 +1,25 @@
 export type { CardProps } from './Card.js';
-export { Card } from './Card.js';
-export { CardContent } from './CardContent';
-export { CardFooter } from './CardFooter';
-export { CardHeader } from './CardHeader';
+
+import { Card as CardComponent } from './Card.js';
+import { CardContent as CardContentComponent } from './CardContent';
+import { CardFooter as CardFooterComponent } from './CardFooter';
+import { CardHeader as CardHeaderComponent } from './CardHeader';
+
+type CardCompound = typeof CardComponent & {
+  CardContent: typeof CardContentComponent;
+  CardFooter: typeof CardFooterComponent;
+  CardHeader: typeof CardHeaderComponent;
+};
+
+// Attach statics to the FINAL component (after forwardRef/memo/etc)
+export const Card: CardCompound = Object.assign(CardComponent, {
+  CardContent: CardContentComponent,
+  CardFooter: CardFooterComponent,
+  CardHeader: CardHeaderComponent,
+});
+
+export {
+  CardContentComponent as CardContent,
+  CardFooterComponent as CardFooter,
+  CardHeaderComponent as CardHeader,
+};
